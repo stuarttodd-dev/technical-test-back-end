e2a258e498b33ca52e47ece7b0d35a03f38cad96
Stuart Todd <stuarttodd444@gmail.com>
2023-08-11 22:21:12 +0100

    add failing action class tests

---
+++ tests/Unit/ActionClassTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ActionClassTest extends TestCase
+{
+    public function test_get_component_grades_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_components_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_component_types_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_farms_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_farm_turbines_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_grades_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_grade_types_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_inspection_grades_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_inspections_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_turbine_components_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_turbine_inspections_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_get_turbines_returns_expected_response()
+    {
+        $this->assertTrue(false);
+    }
+}
