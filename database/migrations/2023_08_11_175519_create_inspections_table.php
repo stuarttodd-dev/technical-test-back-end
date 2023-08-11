d5b3e03da767c316d5daedaf82b775a4aa4892d5
Stuart Todd <stuarttodd444@gmail.com>
2023-08-11 19:14:27 +0100

    fix inspection bug

--- database/migrations/2023_08_11_175519_create_inspections_table.php
+++ database/migrations/2023_08_11_175519_create_inspections_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Turbine;
+
 return new class extends Migration
 {
     /**
