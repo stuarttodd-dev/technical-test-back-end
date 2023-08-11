ac2f9185be215ad25e7cfab8bc66aaf53ddc4d7c
Stuart Todd <stuarttodd444@gmail.com>
2023-08-11 18:55:44 +0100

    add seeders, models and factories

---
+++ database/seeders/FarmSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FarmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
