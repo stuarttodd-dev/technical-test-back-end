76b80540ed33ec3f219fb76d1f1d56bce5ac4c29
Stuart Todd <stuarttodd444@gmail.com>
2023-08-11 19:03:20 +0100

    GradeType migration / model

--- database/migrations/2023_08_11_175504_create_grade_types_table.php
+++ database/migrations/2023_08_11_175504_create_grade_types_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('grade_types', function (Blueprint $table) {
             $table->id();
+            $table->integer('level')->default(1);
+            $table->string('name', 255);
             $table->timestamps();
         });
     }
